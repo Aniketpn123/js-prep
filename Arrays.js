3de5930d180bf6435c625b427bd66bac9343c108
aniketnakhate <aniketpn123@gmail.co>
2026-02-07 23:17:55 +0530

    variable file added

--- Arrays.js
+++ Arrays.js
@@ -110,3 +110,73 @@ console.log( 0 == '');
 console.log('0' == '');
 
 
+// exercises
+
+//1.
+let frutis= ["apple","pear","Orange"];
+
+let cart = fruits;
+
+cart.push("banna");
+
+console.log(fruits);
+
+//2,
+
+let styles =["jazz", "Blues"];
+
+styles.push("Rock-n-Roll");
+let mid =Math.floor((styles.length-1)/2);
+styles[mid] = "Classics";
+styles.shift();
+styles.unshift("Rap","Reggae");
+console.log(styles);
+
+ // 3.
+
+  arr=["a","b"];
+
+ arr.push(function (){console.log(this);
+ })
+
+ arr[2]();
+
+ //4.
+
+ function sumInput(arr){
+    let sum =0;
+   for(let i of arr){
+       if( i !== "" && i !== 'A')
+       {
+        sum += i;      
+       }
+       else{
+        break;
+       }
+   }
+   console.log(sum);
+   
+ }
+ sumInput([1,3,5,6,7,'A',4,"",35,22,5]);
+
+ // 5. 
+
+ function sumMax(arr){
+    let max =0;
+    let submax =0;
+    for(let i =0 ; i < arr.length-1; i++){
+        //submax= max;
+        submax += (arr[i]); 
+        //max = arr[i] + arr[i + 1];
+
+        
+        if( submax < max)
+        {
+            //max = submax;
+            submax =0;
+        }
+    }
+    console.log(submax);
+ }
+
+ sumMax([-1, 2, 3, -9]);
---
+++ varibales.js
@@ -0,0 +1,13 @@
+let message ;
+
+message ='hello';
+let message1 ='hello',age =25,user='john';
+
+message= 'name';
+message ='nickname';
+console.log(message);
+
+var message2 = 'hello';
+let $ =4;
+let _=3;
+let nameOfYou='aniekt';
